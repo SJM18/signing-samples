c674c488300ee44435b34f39ce051a9ea7c1d6a4
Máté Schunk <schunk.mate@dexter.hu>
2020-10-30 23:59:51 +0100

    Create JWT sample implementation - Add JWT implementation - Add sample certificate files - Add sample data to sign and verify

---
+++ SigningSamples/SigningSamples/ISigningSample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigningSamples
+{
+    interface ISigningSample
+    {
+        public void ReadCertification();
+
+        public string Sign(string input);
+
+        public bool Verify(string signedInput);
+    }
+}
--- SigningSamples/SigningSamples/Program.cs
+++ SigningSamples/SigningSamples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace SigningSamples
 {
@@ -6,7 +7,31 @@ namespace SigningSamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region JWT case
+            JWTSigningSample jwtSigningSample = new JWTSigningSample();
+
+            jwtSigningSample.ReadCertification();
+
+            JwtHeader jwtHeader = new JwtHeader();
+            JwtPayload jwtPayload = new JwtPayload();
+            jwtPayload.Add("sampleData1", "sampleDataContent1");
+            jwtPayload.Add("sampleData2", "sampleDataContent2");
+            jwtPayload.Add("sampleData3", "sampleDataContent3");
+
+            var secToken = new JwtSecurityToken(jwtHeader, jwtPayload);
+            var handler = new JwtSecurityTokenHandler();
+
+            var unsignedTokenString = handler.WriteToken(secToken);
+
+            string signedTokenString = jwtSigningSample.Sign(unsignedTokenString);
+
+            if(jwtSigningSample.Verify(signedTokenString))
+            {
+                Console.WriteLine("JWT Verified successfully");
+            }
+
+            #endregion
+
         }
     }
 }
